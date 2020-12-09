d1cbd2f8f02713402849d7f10b0e7e4ae5bdfab8
marwa Radwan <marwaradwan872@hotmail.com>
2020-12-09 14:18:35 +0200

    session 3

--- session 3/js/custom.js
+++ session 3/js/custom.js
@@ -5,17 +5,17 @@
 //p .x a 
 // p.textContent ="marwa"
 tasks = JSON.parse(localStorage.getItem('tasks')) || []
-taskTypeas = ['t1', 't2', 't3']
+taskTypes = ['t1', 't2', 't3']
 temp =""
-taskTypeas.forEach(type=>{
+taskTypes.forEach(type=>{
     temp  +=`<option value=${type}>${type}</option>`
 })
 document.querySelector('select').innerHTML+=temp
 document.querySelector("#showForm").addEventListener('click', function(e){
     e.target.textContent == "Add New Task"? e.target.textContent="Hide Form": e.target.textContent="Add New Task";
+
     document.querySelector('#addTaskForm').classList.toggle('d-none')    
 })
-
 document.querySelector("#addTaskData").addEventListener('submit',function(e){
     e.preventDefault()
     data = e.target.elements
@@ -30,8 +30,11 @@ document.querySelector("#addTaskData").addEventListener('submit',function(e){
     tasks.push(task)
     localStorage.setItem('tasks', JSON.stringify(tasks))
     e.target.reset()
-})
+    displayTasks()
+    document.querySelector("#showForm").textContent="Add New Task"
+    document.querySelector('#addTaskForm').classList.add('d-none')    
 
+})
 const newElement = function(eleType, eleContent, eleClasses, eleContentType='txt'){
     ele = document.createElement(eleType)
     if(eleContentType=='txt') ele.textContent = eleContent
@@ -39,23 +42,32 @@ const newElement = function(eleType, eleContent, eleClasses, eleContentType='txt
     eleClasses.forEach(c=> ele.classList.add(c))
     return ele
 }
-
 const displayTasks = function(){
-    tasks.forEach(task => {
+    document.querySelector('#tasks div').textContent=""
+    tasks.forEach( (task, i) => {
         const bgColor = task.status ? 'bg-warning':'bg-success'
         const article = newElement('article','',[bgColor, 'p-3', 'col-md-3', 'col-12', 'my-4', 'text-white'])
         const p = newElement('p', task.taskContent, [])
         const h2 = newElement('h2', task.taskTitle, ['h4'])
         const btnDel = newElement('button', 'delete', ['btn', 'btn-danger'])
         const btnEdit = newElement('button', 'Edit', ['btn','btn-info'])
-        // article.appendChild(h2)
-        // article.appendChild(p)
-        // article.appendChild(btnDel)
-        // article.appendChild(btnEdit)
-        article.innerHTML = (h2.outerHTML + p.outerHTML + btnEdit.outerHTML + btnDel.outerHTML)
+        //btnEdit.addEventListener('click',function(e){console.log(i)})
+        article.appendChild(h2)
+        article.appendChild(p)
+        article.appendChild(btnDel)
+        article.appendChild(btnEdit)
+        //article.innerHTML = (h2.outerHTML + p.outerHTML + btnEdit.outerHTML + btnDel.outerHTML)
         document.querySelector('#tasks div').appendChild(article)
+        editTask(btnEdit, i)
     });
 
+}
+editTask = function(btn, i){
+    btn.addEventListener('click', function(e){
+        tasks[i].status = !tasks[i].status
+        localStorage.setItem('tasks',JSON.stringify(tasks))
+        displayTasks()
+    })
 }
 
 
